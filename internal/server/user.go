7d54aadb9565c3e34e1d0bf081e796a394a6ea85
mbelesiu <matt.belesiu@gmail.com>
2023-08-28 12:57:07 -0400

    users in, setting up db ft. docker

--- internal/server/user.go
+++ internal/server/user.go
@@ -12,25 +12,25 @@ func signUp(ctx *gin.Context) {
 	user := new(store.User)
 	if err := ctx.Bind(user); err != nil {
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"err": err.Error()})
+		return
 	}
 	store.Users = append(store.Users, user)
 	ctx.JSON(http.StatusOK, gin.H{
 		"msg": "Signed up successfully.",
-		"jwt": "123456789",
+		"jwt": "123456789", //TODO, implement real JWT
 	})
 }
 
 func signIn(ctx *gin.Context) {
-
 	user := new(store.User)
 	if err := ctx.Bind(user); err != nil {
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"err": err.Error()})
+		return
 	}
-
 	for _, u := range store.Users {
 		if u.Username == user.Username && u.Password == user.Password {
 			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "Signed up successfully.",
+				"msg": "Signed in successfully.",
 				"jwt": "123456789",
 			})
 			return
